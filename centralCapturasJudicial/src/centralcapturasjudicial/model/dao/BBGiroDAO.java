2da3f20ab6d5b0735aeba3b9c9dbb478166b7259
f3295813 <f3295813@escta091915d942.intrabb.bb.com.br>
2019-02-01 14:35:14 -0200

    Commit método aberturaTeto

--- centralCapturasJudicial/src/centralcapturasjudicial/model/dao/BBGiroDAO.java
+++ centralCapturasJudicial/src/centralcapturasjudicial/model/dao/BBGiroDAO.java
@@ -6,9 +6,15 @@
 package centralcapturasjudicial.model.dao;
 
 import centralcapturasjudicial.model.entity.bbgiro.OperacaoGiro;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 /**
  *
@@ -19,9 +25,11 @@ public class BBGiroDAO {
     
     
      public OperacaoGiro save(OperacaoGiro c) throws Exception {
-
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("calculobbreuTestesPU");
+         
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("calculobbreuTestesPU");
         EntityManager em = emf.createEntityManager();
+
+     
 
         try {
             em.getTransaction().begin();
@@ -42,6 +50,34 @@ public class BBGiroDAO {
     
     
     
+public void deletarOperacao(String operacao) throws Exception{
+    
+       EntityManagerFactory emf = Persistence.createEntityManagerFactory("calculobbreuTestesPU");
+        EntityManager em = emf.createEntityManager();
+      
+        
+        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+        
+        CriteriaQuery <OperacaoGiro> criteriaQuery = criteriaBuilder.createQuery(OperacaoGiro.class);
+        Root <OperacaoGiro> root = criteriaQuery.from(OperacaoGiro.class);
+        List <Predicate> p = new ArrayList <Predicate> ();
+        if(operacao != null){
+             p.add(criteriaBuilder.equal(root.get("nrOperacao"), operacao));
+        }
+        if(!p.isEmpty()){
+            Predicate[] pr = new Predicate[p.size()];
+            p.toArray(pr);
+            criteriaQuery.where(pr);    
+        }
+        
+        List<OperacaoGiro> listOpr = em.createQuery(criteriaQuery).getResultList();
+        em.getTransaction().begin();
+        for (OperacaoGiro oprs : listOpr) {
+            em.remove(oprs);
+        }
+        em.getTransaction().commit();
+    }
+
     
     
     
